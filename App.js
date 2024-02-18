921fa21d9c38d8f2fa0f8d2212957cd02a10907c
Jose Rus <jrus166@gmail.com>
2024-02-18 14:07:36 -0500

    Change to realm storage

--- App.js
+++ App.js
@@ -2,23 +2,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './Screens/HomeScreen';
 import SettingsScreen from './Screens/SettingsScreen';
+import { RealmProvider } from '@realm/react';
+import { ItemObject } from './Data/StorageFunctions';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen
-          name='Home'
-          component={HomeScreen}
-        />  
-        
-        <Stack.Screen
-          name='Settings'
-          component={SettingsScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RealmProvider schema={[ItemObject]}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name='Home'
+            component={HomeScreen}
+          />
+
+          <Stack.Screen
+            name='Settings'
+            component={SettingsScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RealmProvider>
   );
 }
